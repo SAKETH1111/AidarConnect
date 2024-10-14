34965c9faf94f0f621b1f70c0da1dfa179c653a6
Saketh D <saketh1111@icloud.com>
2024-10-14 13:43:35 -0700

    Add files via upload

--- aidarformbuilder/src/App.js
+++ aidarformbuilder/src/App.js
@@ -1,13 +1,29 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if user is already logged in
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
+    // Redirect to '/home' if logged in
+    if (isLoggedIn) {
+        navigate('/home');
+    }
+  }, [navigate]);
+
   return (
-    <div>
-      <header>
-        <h1
-          className="text-3xl font-bold underline"
-        >
-          Learn React
-        </h1>
-      </header>
+    <div className="flex flex-col min-h-screen bg-rose-100">
+      <Header />
+      <main className="flex-grow px-6 py-4">
+        <Outlet />  {/* used to render child routes within a parent route */}
+      </main>
+      <Footer />
     </div>
   );
 }
--- aidarformbuilder/src/index.js
+++ aidarformbuilder/src/index.js
@@ -2,10 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './components/Login';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Survey from './components/Survey';
+import Welcome from './components/Welcome';
+import Profile from './components/Profile';
+import Logout from './components/Logout';
+import SurveyAnswer from './components/SurveyAnswer';
+import AnsweredSurvey from './components/AnsweredSurvey';
+
+// Define the routes
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />, // Render the Login page at the root path
+  },
+  {
+    path: '/home',
+    element: <App />, // Render the App after successful login
+    children: [
+      {
+        path: '', // This will render Welcome as the default when visiting "/createsurvey"
+        element: <Welcome />,
+      },
+      {
+        path: 'createsurvey',
+        element: <Survey />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+      {
+        path: 'showsurvey',
+        element: <SurveyAnswer />,
+      },
+      {
+        path: 'AnsweredSurvey',
+        element: <AnsweredSurvey />,
+      },
+      {
+        path: 'logout',
+        element: <Logout />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
